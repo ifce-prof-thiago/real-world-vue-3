7ae402470f569d0578c686adf336a15765d31c72
Thiago Felippe <thiago@debian>
2025-11-02 14:58:58 -0300

    Vue Router Essentials - End

---
+++ src/pages/AboutPage.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div class="events">
+    <h1>About Component</h1>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,21 @@
+
+import EventListPage from '@/pages/EventListPage.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'event-list',
+      component: EventListPage
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../pages/AboutPage.vue')
+    }
+  ],
 })
 
 export default router
